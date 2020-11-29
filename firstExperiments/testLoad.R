976ac25e83ec730abf56c021bbe9aa060743aa51
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-11-28 20:42:57 -0800

    more tests.  first use of rcyjs

--- firstExperiments/testLoad.R
+++ firstExperiments/testLoad.R
@@ -1,6 +1,12 @@
 library(neo4r)
 library(RUnit)
+library(RCyjs)
 #------------------------------------------------------------------------------------------------------------------------
+if(!exists("rcy")){
+   title <- "rcy Human1"
+   rcy <- RCyjs(title=title, quiet=TRUE)
+   }
+
 if(!exists("con"))
    con <- neo4j_api$new(url = "http://localhost:7474",
                         user = "neo4j",
@@ -86,4 +92,73 @@ test_simpleReaction <- function()
    checkEquals(x$p$id,   c("M_m00913c", "group72", "ENSG00000172331", "ENSG00000226784", "ENSG00000164708","ENSG00000171314"))
 
 } # test_simpleReaction
+#------------------------------------------------------------------------------------------------------------------------
+# two substrates, three products
+test_moderatelyComplexReaction <- function()
+{
+   message(sprintf("--- test_moderatelyComplexReaction"))
+    "M_m00674c" ->  "substrateOf" ->  "R_HMR_4365" ->  "produces" ->  "M_m00913c"
+
+  # R_HMR_4365 produces M_m00913c substrateRor "R_HMR_3839" produces "M_m02553c" substrateFor R_HMR_3856"
+  # R_HMR_3839 has two substrates, 3 products, and 2 or-enzymes
+
+   query <- "match(n)-[r:substrateOf]-(m:Reaction{id: 'R_HMR_3839'})-[q:produces]->(p) return n, r, m, q, p;"
+   x <- lapply(call_neo4j(query, con), function(element) unique(as.data.frame(element)))
+   checkEquals(names(x), c("n", "r", "m", "q", "p"))
+   checkEquals(sort(x$n$id), sort(c("M_m02552c", "M_m00913c")))
+   checkEquals(x$r$type, "substrateOf")
+   checkEquals(x$m$id,   "R_HMR_3839")
+   checkEquals(x$q$type, "produces")
+   checkEquals(x$p$id,   c("M_m02553c", "M_m02039c", "M_m00914c"))
+
+} # test_moderatelyComplexReaction
+#------------------------------------------------------------------------------------------------------------------------
+# from the preceeding test ("moderatelyComplexReaction") we now that
+#   M_m00674c and M_m00914c are substrate and product, respectively, of two reactions
+#   connected via M_m00913c as a bridging product/substrate of an intervening reaction:
+#    M_m00674c -> R_HMR_4365 -> M_m00913c -> R_HMR_3839 -> M_m00914c
+# this function tests and demonstrates finding this path
+test_simpleShortestPath <- function()
+{
+   message(sprintf("--- test_simpleShortestPath"))
+
+   query <-
+"match(start:Metabolite{id:'M_m00674c'}), (end:Metabolite{id:'M_m00914c'})
+call gds.alpha.shortestPath.stream({
+  nodeProjection: ['Reaction', 'Metabolite'],
+  relationshipProjection: {
+     produces:    {orientation: 'UNDIRECTED'},
+     substrateOf: {orientation: 'UNDIRECTED'}
+    },
+  startNode: start,
+  endNode: end,
+  relationshipWeightProperty: null
+})
+YIELD nodeId, cost
+RETURN gds.util.asNode(nodeId).id AS name, cost;"
+
+   x <- lapply(call_neo4j(query, con), function(element) unique(as.data.frame(element)))
+   checkEquals(x$name$value, c("M_m00674c", "R_HMR_4365", "M_m00913c",  "R_HMR_3839", "M_m00914c"))
+
+} # test_simpleShortestPath
+#------------------------------------------------------------------------------------------------------------------------
+test_toCytoscapeDataFrames <- function()
+{
+   message(sprintf("--- toCytoscapeDataFrames"))
+     # from test_moderatelyComplexReaction above
+   query <- "match(n)-[r:substrateOf]-(m:Reaction{id: 'R_HMR_3839'})-[q:produces]->(p) return n, r, m, q, p;"
+   x <- lapply(call_neo4j(query, con), function(element) unique(as.data.frame(element)))
+
+   tbl.edges <- data.frame(source=c(x$n$id[1], x$n$id[2], rep(x$m$id, 3)),
+                           target=c(x$m$id, x$m$id, x$p$id[1], x$p$id[2], x$p$id[3]),
+                           interaction=c(x$r$type[1],x$r$type[1], rep(x$q$type[1], 3)),
+                           stringsAsFactors=FALSE)
+
+   g.json <- toJSON(dataFramesToJSON(tbl.edges))
+   addGraph(rcy, g.json)
+   layout(rcy, "cose")
+   fit(rcy)
+   selectNodes(rcy, c(x$n$id[1], x$n$id[2], rep(x$m$id, 3))) #rep(x$m$id, 3))
+
+} # test_toCytoscapeDataFrames
 #------------------------------------------------------------------------------------------------------------------------
