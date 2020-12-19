88ade72834b9736c2a444b9ececb46cd23b2a0b3
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-12-19 08:04:32 -0800

    reaction and species now extracted

--- human1/Human1Parser/R/Human1Parser.R
+++ human1/Human1Parser/R/Human1Parser.R
@@ -30,15 +30,124 @@ Human1Parser = R6Class("Human1Parser",
          private$doc <- xmlParse(filename)
          },
 
-        #' @description
-        #' learn the elements and their count
-        #' @returns a names list of counts
+      #----------------------------------------------------------------------------------------------------
+      #' @description
+      #' learn the elements and their count
+      #' @returns a names list of counts
       getCounts = function(){
-          model <- xmlRoot(private$doc)[["model"]]
-          list(reactions=length(xmlChildren(model[["listOfReactions"]])),
-               species=length(xmlChildren(model[["listOfSpecies"]])))
-         }
+         model <- xmlRoot(private$doc)[["model"]]
+         list(reactions=length(xmlChildren(model[["listOfReactions"]])),
+              species=length(xmlChildren(model[["listOfSpecies"]])),
+              compartments=length(xmlChildren(model[["listOfCompartments"]])),
+              geneProducts=length(xmlChildren(model[["listOfGeneProducts"]])),
+              groups=length(xmlChildren(model[["listOfGroups"]]))
+              )
+         }, # getCounts
 
+      #----------------------------------------------------------------------------------------------------
+      #' @description
+      #' extract one molecular species
+      #' @param n integer the index of the reaction
+      #' @returns two data.frames: species and speciesRefs
+      getSpecies = function(n){
+         species <- getNodeSet(private$doc, sprintf("(//species)[%d]", n))
+         speciesAsList <- as.list(xmlAttrs(species[[1]]))
+
+         id <- speciesAsList[["id"]]
+         name <- speciesAsList[["name"]]
+         compartment <- speciesAsList[["compartment"]]
+         chemicalFormula <- ""
+         if("chemicalFormula" %in% names(speciesAsList))   # sometimes (rarely) missing
+             chemicalFormula <- speciesAsList[["chemicalFormula"]]
+
+         tbl.species <- data.frame(id=id,
+                                   name=name,
+                                   compartment=compartment,
+                                   chemicalFormula=chemicalFormula,
+                                   stringsAsFactors=FALSE)
+         annotation <- getNodeSet(private$doc, sprintf("(//species)[%d]/annotation/*/*/*/*/li", n))
+         external.ids <- as.character(lapply(annotation, xmlAttrs))
+         external.ids <- sub("http://identifiers.org/", "", external.ids)
+         # <li resource="http://identifiers.org/ec-code/1.1.1.1"/>
+         # <li resource="http://identifiers.org/ec-code/1.1.1.71"/>
+         # <li resource="http://identifiers.org/kegg.reaction/R00754"/>
+         # <li resource="http://identifiers.org/bigg.reaction/ALCD2x"/>
+         # <li resource="http://identifiers.org/metanetx.reaction/MNXR95725"/>
+         tokens <- strsplit(external.ids, "/", fixed=TRUE)
+         tbl.refs <- data.frame();
+
+         for(token in tokens)
+             tbl.refs <- rbind(tbl.refs, data.frame(ref=token[1], value=token[2], stringsAsFactors=FALSE))
+
+         return(list(
+             species=tbl.species,
+             speciesRefs=tbl.refs)
+             )
+
+         }, # getSpecies
+      #----------------------------------------------------------------------------------------------------
+      #' @description
+      #' extract one reaction
+      #' @param n integer the index of the reaction
+      #' @returns a list of data.frames: reaction, reactionrefs, reactants, products, a list of genes
+      getReaction = function(n){
+         reaction <- getNodeSet(private$doc, sprintf("(//reaction)[%d]", n))
+         tbl.reaction <- with(as.list(xmlAttrs(reaction[[1]])),
+                              data.frame(id=id,
+                                         name=name,
+                                         reversible=reversible,
+                                         fast=fast,
+                                         lowerFluxBound=lowerFluxBound,
+                                         upperFluxBound=upperFluxBound,
+                                         stringsAsFactors=FALSE))
+
+           #       metaid        sboTerm             id     reversible           fast lowerFluxBound  upperFluxBound
+           #  "R_HMR_3905"  "SBO:0000176"   "R_HMR_3905"        "false"        "false"        "FB2N0"    "FB3N1000"
+
+         annotation <- getNodeSet(private$doc, sprintf("(//reaction)[%d]/annotation/*/*/*/*/li", n))
+         external.ids <- as.character(lapply(annotation, xmlAttrs))
+         external.ids <- sub("http://identifiers.org/", "", external.ids)
+           # <li resource="http://identifiers.org/ec-code/1.1.1.1"/>
+           # <li resource="http://identifiers.org/ec-code/1.1.1.71"/>
+           # <li resource="http://identifiers.org/kegg.reaction/R00754"/>
+           # <li resource="http://identifiers.org/bigg.reaction/ALCD2x"/>
+           # <li resource="http://identifiers.org/metanetx.reaction/MNXR95725"/>
+         tokens <- strsplit(external.ids, "/", fixed=TRUE)
+         tbl.refs <- data.frame();
+         for(token in tokens)
+            tbl.refs <- rbind(tbl.refs, data.frame(ref=token[1], value=token[2], stringsAsFactors=FALSE))
+         reactants <- getNodeSet(private$doc, sprintf("(//reaction)[%d]/listOfReactants/speciesReference", n))
+         tbls.reactants <- lapply(reactants, function(reactant) {
+            attrs <- xmlAttrs(reactant)
+            with(as.list(attrs),
+                 data.frame(species=species, stoichiometry=stoichiometry, constant=constant,
+                            stringsAsFactors=FALSE))
+            })
+         tbl.reactants <- do.call(rbind, tbls.reactants)
+         products  <- getNodeSet(private$doc, sprintf("(//reaction)[%d]/listOfProducts/speciesReference", n))
+         tbls.products <- lapply(products, function(product) {
+            attrs <- xmlAttrs(product)
+            with(as.list(attrs),
+                 data.frame(species=species, stoichiometry=stoichiometry, constant=constant,
+                            stringsAsFactors=FALSE))
+            })
+          tbl.products <- do.call(rbind, tbls.products)
+            # todo: distinguish "and" genes from "or" genes from single-item geneProductRef
+        genes.raw.chosen  <- getNodeSet(private$doc, sprintf("(//reaction)[%d]/geneProductAssociation//geneProductRef", n))
+        printf(" reaction %d,    genes found: %d", n, length(genes.raw.chosen))
+        genes <- unlist(lapply(genes.raw.chosen, function(gene) {
+           attrs <- xmlAttrs(gene)
+           attrs[["geneProduct"]]
+           }))
+
+         return(list(
+             reaction=tbl.reaction,
+             reactionRefs=tbl.refs,
+             reactants=tbl.reactants,
+             products=tbl.products,
+             genes=genes
+             ))
+        } # getReaction
      ) # public
   ) # class
 
--- human1/Human1Parser/inst/unitTests/test_Human1Parser.R
+++ human1/Human1Parser/inst/unitTests/test_Human1Parser.R
@@ -13,22 +13,65 @@ runTests <- function()
 {
    test_ctor()
    test_counts()
+   test_getReaction()
+   test_getSpecies()
 
 } # runTests
 #------------------------------------------------------------------------------------------------------------------------
 test_ctor <- function()
 {
    message(sprintf("--- test_ctor"))
-   printf("class of object: %s", class(hp))
+   checkTrue("Human1Parser" %in% class(hp))
 
 } # test_ctor
 #------------------------------------------------------------------------------------------------------------------------
 test_counts <- function()
 {
    message(sprintf("--- test_counts"))
-   print(hp$getCounts())
+   counts <- hp$getCounts()
+   checkTrue(all(c("compartments", "geneProducts", "groups", "reactions", "species") %in% names(counts)))
+   checkEquals(counts$reactions, 13096)
+   checkEquals(counts$species, 8400)
+   checkEquals(counts$compartments, 9)
+   checkEquals(counts$geneProducts, 3626)
+   checkEquals(counts$groups, 142)
 
-} # test_ctor
+} # test_counts
+#------------------------------------------------------------------------------------------------------------------------
+# the drug acarbose inhibits this reaction, making it interesting to longevity researchers
+test_getReaction <- function()
+{
+   message(sprintf("--- test_getReaction"))
+
+   r <- hp$getReaction(8531)
+   checkTrue(all(c("reaction", "reactionRefs", "reactants", "products", "genes") %in% names(r)))
+   checkEquals(r$reaction$id, "R_O16G1e")
+   checkEquals(r$reaction$name, "Oligo-1, 6-Glucosidase (Glygn4 -> Glygn5), Extracellular")
+   tbl.refs <- r$reactionRefs
+   checkEquals(dim(tbl.refs), c(3, 2))
+   checkEquals(sort(tbl.refs$value), c("3.2.1.10", "MNXR102083","O16G1e"))
+   checkEquals(r$reactants$species, c("M_m02040s", "M_glygn4_s"))
+   checkEquals(r$products$species, c("M_m01965s", "M_glygn5_s"))
+   checkEquals(r$genes, "ENSG00000090402")
+
+} # test_getReaction
+#------------------------------------------------------------------------------------------------------------------------
+# M_m01965s, extracellular glucose C6H12O6
+test_getSpecies <- function()
+{
+   message(sprintf("--- test_getSpecies"))
+
+   r <- hp$getSpecies(3359)
+   checkEquals(sort(names(r)), c("species", "speciesRefs"))
+   checkEquals(r$species$id, "M_m01965s")
+   checkEquals(r$species$name, "glucose")
+   checkEquals(r$species$compartment, "s")  # extracellular
+   checkEquals(r$species$chemicalFormula, "C6H12O6")
+   checkTrue(all(c("bigg.metabolite", "kegg.compound", "hmdb", "chebi", "pubchem.compound", "metanetx.chemical", "metanetx.chemical")
+                 %in% r$speciesRefs$ref))
+   checkTrue(all(c("glc__D", "C00031", "HMDB00122", "CHEBI:4167", "5793", "MNXM41", "MNXM99") %in% r$speciesRefs$value))
+
+} # test_getSpecies
 #------------------------------------------------------------------------------------------------------------------------
 if(!interactive())
     runTests()
